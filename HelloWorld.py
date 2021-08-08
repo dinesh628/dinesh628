ca02b8606b1a8341a319cfb0c5360bbd397591cb
Dinesh Kumar Regonda <dineshkumarregonda@DineshKumar-Regondas-MacBook-Pro.local>
2021-08-08 08:04:21 -0500

    comments

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,14 @@
-print ('Hello, world!')
+#!/usr/bin/env python3.6
+
+# The above line makes the file executable with python3.6 given we chmod u+x file.py and execute it as ./file.py
+# mainly useful while executing large script files
+
+# This is a full line comment
+
+print ('Hello, world!') # This is a trailing comment
+
+"""
+This is not a block comment.
+Python does not have a block comment.
+This is a multiline string and costs memory.
+"""
